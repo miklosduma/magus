30b694ff2911d323204b9f7bcf9d52fed47b696f
miklosduma <miklos@sparkl.com>
2018-01-14 13:58:15 +0000

    Added comments, constants and removed not used funs

--- magus_kalkulator/interface_karakterek.py
+++ magus_kalkulator/interface_karakterek.py
@@ -5,6 +5,8 @@ from validate import validate_integer, validate_string
 
 
 FIELD_WIDTH = 10
+FIELD_COLOR = 'white'
+FIELD_COLOR_ERROR = 'red'
 
 KARAKTER_PANEL_COLUMN = 0
 KARAKTER_PANEL_ROW = 0
@@ -21,6 +23,20 @@ NEV_LABEL = 'Nev'
 
 SFE_FRAME_TITLE = 'SFE'
 SFE_LABEL = 'Max SFE'
+
+SFE_SHORTCUT_LABEL = 'Mindenhol'
+
+SFE_FEJ_LABEL = 'Fej SFE'
+SFE_FEJ_PARTS = ['Arc', 'Nyak', 'Koponya']
+
+SFE_TORZS_LABEL = 'Torzs SFE'
+SFE_TORZS_PARTS = ['Mellkas', 'Has', 'Agyek']
+
+SFE_KAR_LABEL = 'Kar SFE'
+SFE_KAR_PARTS = ['Vall', 'Felkar', 'Konyok', 'Alkar', 'Csuklo', 'Kezfej']
+
+SFE_LAB_LABEL = 'Lab SFE'
+SFE_LAB_PARTS = ['Comb', 'Terd', 'Labszar', 'Boka', 'Labfej']
 
 BUTTONS_FRAME_TITLE = 'Buttons'
 ADD_BUTTON = 'Add'
@@ -48,7 +64,9 @@ def copy_value_to_keys(my_map, key, *new_keys):
 
 
 class KarakterPage(ttk.Frame):
-
+    """
+    Adding characters main page.
+    """
     def __init__(self, master, master_gui, width):
         self.master = master
         self.gui_top = master_gui
@@ -60,7 +78,9 @@ class KarakterPage(ttk.Frame):
 
 
 class KarakterPanels(ttk.PanedWindow):
-
+    """
+    Main panel of characters main page.
+    """
     def __init__(self, master, width, orient=VERTICAL):
         ttk.PanedWindow.__init__(self, master, width=width, orient=orient)
         self.gui_top = master.gui_top
@@ -77,7 +97,10 @@ class KarakterPanels(ttk.PanedWindow):
 
 
 class NameFrame(ttk.LabelFrame):
-
+    """
+    Part of characters panel. Contains
+    name field with label.
+    """
     def __init__(self, master):
         self.master = master
         self.gui_top = master.gui_top
@@ -89,7 +112,10 @@ class NameFrame(ttk.LabelFrame):
 
 
 class FieldsFrame(ttk.LabelFrame):
-
+    """
+    Part of characters panel. Contains
+    ep and fp fields with labels.
+    """
     def __init__(self, master):
         self.master = master
         self.gui_top = master.gui_top
@@ -107,10 +133,13 @@ class FieldsFrame(ttk.LabelFrame):
 
 
 class SfeFrame(ttk.LabelFrame):
-
+    """
+    Part of characters panel. Contains the
+    main sfe box and all body-part sfe boxes.
+    """
     def __init__(self, master):
         ttk.LabelFrame.__init__(self, master, text=SFE_FRAME_TITLE)
-        self.sfe_label = Label(self, text='Mindenhol')
+        self.sfe_label = Label(self, text=SFE_SHORTCUT_LABEL)
         self.sfe_field = CharacterValueField(self, validate_integer, width=2)
         self.sfe = {}
         self.master = master
@@ -118,24 +147,26 @@ class SfeFrame(ttk.LabelFrame):
         self.sfe_field.grid(row=0, column=1)
 
         self.fej_sfe = SfePartFrame(
-            self, 'Fej SFE', ['Arc', 'Nyak', 'Koponya'])
+            self, SFE_FEJ_LABEL, SFE_FEJ_PARTS)
         self.fej_sfe.grid(row=3, columnspan=5, sticky=(N, W))
 
         self.torzs_sfe = SfePartFrame(
-            self, 'Torzs SFE', ['Mellkas', 'Has', 'Agyek'])
+            self, SFE_TORZS_LABEL, SFE_TORZS_PARTS)
         self.torzs_sfe.grid(row=3, column=5, columnspan=5, sticky=(N, W))
 
-        self.kar_sfe = SfePartFrame(self, 'Kar SFE', ['Vall', 'Felkar', 'Konyok', 'Alkar', 'Csuklo', 'Kezfej'],
+        self.kar_sfe = SfePartFrame(self, SFE_KAR_LABEL, SFE_KAR_PARTS,
                                     limb=True)
         self.kar_sfe.grid(row=4, columnspan=5, sticky=(N, W))
 
         self.lab_sfe = SfePartFrame(
-            self, 'Lab SFE', ['Comb', 'Terd', 'Labszar', 'Boka', 'Labfej'], limb=True)
+            self, SFE_LAB_LABEL, SFE_LAB_PARTS, limb=True)
         self.lab_sfe.grid(row=4, column=5, columnspan=5, sticky=(N, W))
 
 
 class ButtonsFrame(ttk.LabelFrame):
-
+    """
+    Part of characters panel. Contains buttons.
+    """
     def __init__(self, master):
         ttk.LabelFrame.__init__(self, master, text=BUTTONS_FRAME_TITLE)
         self.master = master
@@ -149,7 +180,9 @@ class ButtonsFrame(ttk.LabelFrame):
 
 
 class CharacterAddButton(Button):
-
+    """
+    Adds new character on clicking.
+    """
     def __init__(self, master, text, karakterek):
         self.master = master
         self.name = self.master.master.name_frame.name_field
@@ -160,16 +193,29 @@ class CharacterAddButton(Button):
         self.bind('<Button-1>', self.add_character)
 
     def add_character(self, event):
+        """
+        Function executed on clicking Add button.
+
+        Either adds a new character or returns error
+        message if validation fails or character already
+        exists.
+        """
+
+        # Get sfe fields from sfe frame
         sfe_map = self.master.master.sfe_frame.sfe
 
+        # Build sfe map from fields. If any field fails
+        # validation the character is not added
         new_sfe_map = {}
         for key, sfe_field in sfe_map.items():
             success, value = sfe_field.validate()
 
+            # Handle validation failures
             if not success:
                 self.messages.write_message(value)
                 return
 
+            # Add field's current value to sfe map
             else:
                 new_sfe_map[key] = value
 
@@ -177,10 +223,12 @@ class CharacterAddButton(Button):
         new_sfe_map = copy_value_to_keys(new_sfe_map,
                                          'Mellkas', 'Jkulcs', 'Bkulcs')
 
+        # Get name, ep and fp fields
         name_field = self.name
         ep_field = self.fields.ep_field
         fp_field = self.fields.fp_field
 
+        # Validate each, and add their values to list
         values = []
         for field in [name_field, ep_field, fp_field]:
             success, value = field.validate()
@@ -193,6 +241,7 @@ class CharacterAddButton(Button):
 
         [name, ep, fp] = values
 
+        # Add new character. Addition fails if character already exists.
         success, msg = self.karakterek.add_karakter(
             name, ep, new_sfe_map, fp=fp)
 
@@ -208,7 +257,9 @@ class CharacterAddButton(Button):
 
 
 class CharactersGetButton(Button):
-
+    """
+    Button that lists all characters already added.
+    """
     def __init__(self, master, text, karakterek):
         self.master = master
         self.karakterek = karakterek
@@ -217,10 +268,11 @@ class CharactersGetButton(Button):
         self.bind('<Button-1>', self.get_characters)
 
     def get_characters(self, event):
+        """
+        Function executed on clicking Get button.
+        Lists all characters already added.
+        """
         all_characters = self.karakterek.get_all_karakters()
-
-        self.master.master.sfe_frame.fej_sfe.get_part_sfe()
-        self.master.master.sfe_frame.lab_sfe.get_part_sfe()
 
         if not all_characters:
             msg = NO_CHARACTERS
@@ -233,39 +285,59 @@ class CharactersGetButton(Button):
 
 
 class CharacterValueField(Entry):
-
+    """
+    Basic field type used to store character values.
+    """
     def __init__(self, master, validate_fun, width=FIELD_WIDTH):
         """
         Initialise input field.
+         - validate_fun:
+            A function used to validate the field's input value.
         """
-        # Varible for entered damage
+        # Varible for entered value
         self.value = StringVar(master)
         self.value.set('')
-        self.value.trace('w', self.print_on_change)
+        self.value.trace('w', self.follow_changes)
         self.validator = validate_fun
 
         # Entry field
         Entry.__init__(self, master, textvariable=self.value,
                        width=width)
 
-    def print_on_change(self, *args):
+    def follow_changes(self, *args):
+        """
+        Traces changes to the input value
+        of the field.
+        """
+        # Get current background color of field.
         color = self.cget('bg')
 
-        if color == 'red':
-            self.config(bg='white')
+        # It turns red on validation failures.
+        # Any subsequent change must turn the color back
+        # to the default color.
+        if color == FIELD_COLOR_ERROR:
+            self.config(bg=FIELD_COLOR)
 
         print(self.value.get())
 
     def validate(self):
+        """
+        Calls assigned validator function on the
+        value of the field.
+
+        If validation fails, turns the field to red.
+        """
         success, value = self.validator(self.value.get())
 
         if not success:
-            self.config(bg='red')
+            self.config(bg=FIELD_COLOR_ERROR)
         return success, value
 
 
 class SfePartFrame(ttk.LabelFrame):
-
+    """
+    Frame child type of main sfe frame. E.g. Fej or Lab frame.
+    """
     def __init__(self, master, text, fields, limb=False):
         ttk.LabelFrame.__init__(self, master, text=text)
         self.is_limb = limb
@@ -273,7 +345,7 @@ class SfePartFrame(ttk.LabelFrame):
         self.master = master
         self.total_sfe = self.master.sfe_field.value
         self.sfe_field = CharacterValueField(self, validate_integer, width=2)
-        Label(self, text='Total').grid(row=0, column=0, sticky=W)
+        Label(self, text=SFE_SHORTCUT_LABEL).grid(row=0, column=0, sticky=W)
         self.sfe_field.grid(row=0, column=1, sticky=W)
         self.master = master
         self.fields = self.master.sfe
@@ -283,9 +355,19 @@ class SfePartFrame(ttk.LabelFrame):
         self.sfe_field.value.trace('w', self.follow_total)
 
     def follow_master(self, *_args):
+        """
+        Each child frame has a shortcut value that all
+        sub parts follow. E.g. setting Mindenhol in Lab frame
+        will change both JLabszar and BBoka to the same value.
+        """
         self.sfe_field.value.set(self.total_sfe.get())
 
     def follow_total(self, *_args):
+        """
+        There is a master shortcut value that all other
+        fields follow. Setting the value of it changes
+        all other sfe field values.
+        """
         for key, value in self.fields.items():
 
             if key in self.local_fields:
@@ -293,9 +375,15 @@ class SfePartFrame(ttk.LabelFrame):
                 value.value.set(total)
 
     def place_sfe_fields(self, fields):
+        """
+        Function to place multiple connected fields assigning
+        each a label.
+        """
         column = 0
         row = 1
 
+        # If field is a limb sfe (e.g. Labszar or Boka)
+        # Jobb and Bal label will be added
         if self.is_limb:
             jobb_bal = Label(self, text='Jobb/Bal')
             jobb_bal.grid(row=row, column=column + 1)
@@ -303,6 +391,8 @@ class SfePartFrame(ttk.LabelFrame):
 
         for field in fields:
 
+            # If field is a limb sfe (e.g. Labszar or Boka)
+            # it will be added twice both as Jlabszar and Blabszar
             if self.is_limb:
                 jobb_field = 'J{}'.format(field.lower())
                 bal_field = 'B{}'.format(field.lower())
@@ -317,6 +407,8 @@ class SfePartFrame(ttk.LabelFrame):
                 sfe_field_value = CharacterValueField(
                     self, validate_integer, width=2)
 
+                # Right (jobb) limbs are left aligned,
+                # left (bal) limbs are right aligned
                 if sfe_fields.index(sfe_field) == 0:
                     direction = W
                 else:
@@ -328,17 +420,3 @@ class SfePartFrame(ttk.LabelFrame):
                 self.local_fields.append(sfe_field)
 
             row += 1
-
-    def get_part_sfe(self):
-        values = []
-
-        for key in self.fields.keys():
-            values.append(self.fields[key].value.get())
-        return values
-
-    def add_sfe(self):
-        for key, value in self.fields.items():
-            if 'get' not in dir(value):
-                print(dir(value))
-            elif value.get():
-                self.fields[key] = value.get()
