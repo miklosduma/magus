1c86ca19f3dbf6121868dd5d19acee29901bfa64
miklosduma <miklos@sparkl.com>
2018-01-14 13:16:48 +0000

    Added fun for retrieving sfe and updated tests

--- magus_kalkulator/sebzes.py
+++ magus_kalkulator/sebzes.py
@@ -36,16 +36,29 @@ def calculate_damage(sfe, damage, atutes, tulutes=False):
     return ep_damage, fp_damage
 
 
+def get_sfe_per_part(sfe_map, bp_list):
+    """
+    Tries to retrieve the sfe per body part.
+    The corresponding body part will be either
+    the second or the third element of the body parts list.
+    """
+    main_part, penalty_part, sfe_part = bp_list
+
+    try:
+        sfe = sfe_map[sfe_part]
+    except KeyError:
+        sfe = sfe_map[penalty_part]
+
+    return sfe
+
+
 def return_penalty(sfe, damage, atutes, max_ep, wtype, tulutes=False):
     """
     Gets the penalty associated with a damage.
     """
-    main_part, penalty_part, sfe_part = pick_sub_parts()
-    try:
-        sfe = sfe[sfe_part]
-    except KeyError:
-        sfe = sfe[penalty_part]
-
+    body_parts_list = pick_sub_parts()
+    sfe = get_sfe_per_part(sfe, body_parts_list)
+    main_part, penalty_part, sfe_part = body_parts_list
     ep_loss, fp_loss = calculate_damage(sfe, damage, atutes, tulutes=tulutes)
     result = calculate_penalty(ep_loss, max_ep, wtype, main_part, penalty_part)
     return {
--- tests/test_sebzes.py
+++ tests/test_sebzes.py
@@ -10,6 +10,15 @@ test_data_calculate = [
     (0, 7, 2, False, (1, 7))
 ]
 
+test_sfe_map = {
+    'Bboka': 4, 'Jfelkar': 4, 'Jlabszar': 4, 'Bkulcs': 4, 'Jlabfej': 4,
+    'Has': 4, 'Jvall': 4, 'Jkonyok': 4, 'Bcomb': 4, 'Nyak': 4, 'Jkezfej': 4,
+    'Jterd': 4, 'Bcsuklo': 4, 'Bkonyok': 4, 'Balkar': 4, 'Jkulcs': 4,
+    'Bfelkar': 4, 'Agyek': 4, 'Jcomb': 4, 'Blabszar': 4, 'Jboka': 4,
+    'Jalkar': 4, 'Bvall': 4, 'Mellkas': 4, 'Bterd': 4, 'Jcsuklo': 4,
+    'Blabfej': 4, 'Arc': 4, 'Bkezfej': 4, 'Koponya': 4
+}
+
 
 @pytest.mark.parametrize('sfe,damage,atutes,tulutes,expected',
                          test_data_calculate)
@@ -19,13 +28,13 @@ def test_calculate_damage(sfe, damage, atutes, tulutes, expected):
 
 
 def test_return_penalty():
-    result = return_penalty(7, 21, 3, 14, 'Szur')
+    result = return_penalty(test_sfe_map, 21, 3, 14, 'Szur')
     expected_keys = ['ep_loss', 'fp_loss', 'hit_target', 'penalty']
     actual_keys = result.keys()
     assert all(x in actual_keys for x in expected_keys)
 
-    assert result['ep_loss'] == 3
-    assert result['fp_loss'] == 17
+    assert result['ep_loss'] == 4
+    assert result['fp_loss'] == 20
 
     target_parts = len(result['hit_target'])
     assert target_parts == 3
