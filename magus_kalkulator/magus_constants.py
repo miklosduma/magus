c92b5d186481bccac942e09a61353f6e549d9149
miklosduma <miklos@sparkl.com>
2018-01-22 08:50:02 +0000

    Finished body parts

--- magus_kalkulator/magus_constants.py
+++ magus_kalkulator/magus_constants.py
@@ -67,23 +67,36 @@ LARM_LIST = [LARM, [
     [LARM, LLOWERARM], [LARM, LLOWERARM],
     [LARM, LWRIST], [LARM, LHAND]]]
 
-
-BODY_PARTS = {
-
-    'Jlab': [
-        ('Jlab', 'Jcomb', 5),
-        ('Jlab', 'Jterd', 1),
-        ('Jlab', 'Jlabszar', 2),
-        ('Jlab', 'Jboka', 1),
-        ('Jlab', 'Jlabfej', 1)],
-    'Blab': [
-        ('Blab', 'Bcomb', 5),
-        ('Blab', 'Bterd', 1),
-        ('Blab', 'Blabszar', 2),
-        ('Blab', 'Bboka', 1),
-        ('Blab', 'Blabfej', 1)],
-
-}
-
+# Right leg parts
 RLEG = 'Jlab'
+RTHIGH = 'Jcomb'
+RKNEE = 'Jterd'
+RSHIN = 'Jlabszar'
+RANKLE = 'Jboka'
+RFOOT = 'Jlabfej'
+
+RLEG_LIST = [RLEG, [
+    [RLEG, RTHIGH], [RLEG, RTHIGH],
+    [RLEG, RTHIGH], [RLEG, RTHIGH],
+    [RLEG, RTHIGH], [RLEG, RKNEE],
+    [RLEG, RSHIN], [RLEG, RSHIN],
+    [RLEG, RANKLE], [RLEG, RFOOT]
+]]
+
+# Left leg parts
 LLEG = 'Blab'
+
+LTHIGH = 'Jcomb'
+LKNEE = 'Jterd'
+LSHIN = 'Jlabszar'
+LANKLE = 'Jboka'
+LFOOT = 'Jlabfej'
+
+LLEG_LIST = [LLEG, [
+    [LLEG, LTHIGH], [LLEG, LTHIGH],
+    [LLEG, LTHIGH], [LLEG, LTHIGH],
+    [LLEG, LTHIGH], [LLEG, LKNEE],
+    [LLEG, LSHIN], [LLEG, LSHIN],
+    [LLEG, LANKLE], [LLEG, LFOOT]
+]]
+
