4c8d4fa6c3e6cb1f89530330a9ffbaf5114c4a11
miklosduma <miklos@sparkl.com>
2018-08-31 22:07:01 +0200

    Updated penalty tables html representation

--- resources/collapse.js
+++ resources/collapse.js
@@ -1,10 +1,14 @@
-var hidden = []
-var is_hidden = false
+var hidden_dict = {}
 
 
 function collapse(button) {
+	var hidden = []
 
 	var header_cell = button.parentNode
+
+	var id = header_cell + Math.random().toString()
+
+	var text = document.createTextNode('Reset' + ' ' + header_text)
 	
 	header_cell.style.display = 'none'
 	hidden.push(header_cell)
@@ -14,23 +18,22 @@ function collapse(button) {
 	
 	var first_cell = row.childNodes[0]
 
-	var table_body = row.parentNode
-	var body = table_body.parentNode.parentNode
+	insert_reset_button(first_cell, 'reset', id, text)
 
-	var no_of_cols = header_cell.getAttribute('colspan')
-	console.log(no_of_cols)
-
-	var all_rows = table_body.childNodes
+	var all_rows = row.parentNode.childNodes
 
 	for (row_index=0; row_index < all_rows.length; row_index++) {
 		var cells = all_rows[row_index].childNodes
 
 		for (cell_index=0; cell_index < cells.length; cell_index++) {
 			var current_cell = cells[cell_index]
+
 			if (current_cell.hasAttribute('id')){
 				var cell_id = current_cell.getAttribute('id')
+			
 
 				if (cell_id.includes(header_text)){
+
 					current_cell.style.display = 'none'
 					hidden.push(current_cell)
 				}
@@ -38,23 +41,25 @@ function collapse(button) {
 		}
 	}
 
-	if (is_hidden == false) {
-		var revert_button = document.createElement('button')
-		revert_button.setAttribute('onclick', 'revert(this)')
-		revert_button.setAttribute('class', 'reset')
-		var text = document.createTextNode('Reset')
-		revert_button.append(text)
-		body.prepend(revert_button)
-		is_hidden = true
-	}
+	hidden_dict[id] = hidden
+}
+
+
+function insert_reset_button(parent, klass, id, text_node) {
+	var button = document.createElement('button')
+	button.setAttribute('onclick', 'revert(this)')
+	button.setAttribute('class', klass)
+	button.setAttribute('id', id)
+	
+	button.append(text_node)
+	parent.appendChild(button)
 }
 
 
 function revert(button) {
+	var hidden = hidden_dict[button.getAttribute('id')]
 	for (hidden_index=0; hidden_index < hidden.length; hidden_index++) {
 		hidden[hidden_index].style.display = 'table-cell'
 	}
 	button.parentNode.removeChild(button)
-	hidden = []
-	is_hidden = false
 }
